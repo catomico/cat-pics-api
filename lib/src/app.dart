d35cbcc49d04d800c0eb0504319c41ed775c6399
user86 <user86marc@gmail.com>
2021-09-06 06:24:38 +0200

    initialise

---
+++ lib/src/app.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'package:pics/src/widgets/image_list.dart';
+import '/src/models/image_model.dart';
+import 'dart:convert';
+
+class AppWidget extends StatefulWidget {
+  // @override
+  State<StatefulWidget> createState() {
+    return AppWidgetState();
+  }
+}
+
+class AppWidgetState extends State<AppWidget> {
+  // this is an instance variable
+  int counter = 0;
+  String catSpeak = 'mew';
+
+  // make a list data type to hold the multiple instances of image model that the user creates when pressing the button.
+  List<ImageModel> images = [];
+
+  // This method ccould go inside the below widget, but to keep things cleann it is here. Type is void since, it returns nothing, it is only called.
+  void fetchImage() async {
+    counter++;
+    // response variable is not only the json data it is info about the data and has the json data nested inside
+    final response = await get(
+        Uri.parse('https://jsonplaceholder.typicode.com/photos/$counter'));
+    // Creat a new model image instance,
+    // take the json out, and construct a new image model -> imageModel.
+    // so call the named constuctor from json -> fromJson
+    // pass in json from the response object, -> (response
+    // which is availeble on response body -> .body)
+    final imageModel = ImageModel.fromJson(json.decode(response.body));
+    //adds to the list - this will cause the app to re-render, therefore call setState
+    setState(() {
+      images.add(imageModel);
+    });
+  }
+
+  // defines a method that returns, or 'builds', the widgets that my custom AppWidget will show
+  // @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My Cat Pics App'),
+        ),
+        body: ImageList(images),
+        // Column(
+        //   children: [
+        //     Align(
+        //       alignment: Alignment.topCenter,
+        //       child: Text(
+        //         'How many meows?' + '\n$counter ' + '$catSpeak',
+        //         style: TextStyle(fontSize: 20),
+        //       ),
+        //     ),
+        //     ImageList(images)
+        //   ],
+        // ),
+        floatingActionButton: FloatingActionButton(
+          hoverColor: Colors.pink[300],
+          child: Icon(Icons.pets_rounded),
+          // If onpressed is only used to call another function, then use this shorthand. if fetchImage were fetchImage() it would run each time the build method were invoked
+          // onPressed: fetchImage,
+          onPressed: () {
+            setState(() {
+              fetchImage();
+              counter += 1;
+              catSpeak = 'mews';
+            });
+            print('meow?');
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/widgets/image_list.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:pics/src/models/image_model.dart';
+import '../../src/models/image_model.dart';
+
+// The image list holds data that changes, i.e., add to list, but the parent widget of the app state is responsible for the changing data, not the image list itself, and won't re-render itself.
+class ImageList extends StatelessWidget {
+  // instance variable -> a list that will contain images
+  // final is like var but var can change later, final is immutable.
+  // makes sense to use final inside of a stateless widget
+  final List<ImageModel> images;
+
+  // constructor
+  // this. assumes there is an stance variable
+  ImageList(this.images);
+// when the item builder function gets called, the a widget representing an image from the ImageList(this.image) is returned.
+// but the are not provided directly to the builder, below. Instead they are refered by the index
+
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      // number of images to show = all of them
+      itemCount: images.length,
+      itemBuilder: (context, int index) {
+        // return a text element that references the ImageList or imaage[look up the index] and pull out the .url
+        return buildImageStyle(images[index]);
+        // Container(
+        // there can be too much here so take it out to buildImageStyles method below.
+        // decoration: BoxDecoration(
+        //   border: Border.all(color: Colors.pink[300]!),
+        // ),
+        // padding: EdgeInsets.all(20.0),
+        // margin: EdgeInsets.all(20.0),
+        // child: Image.network(images[index].url),
+        // );
+      },
+    );
+  }
+}
+
+// pass in the image Modoel instance add some style, but don't go look for the list of images here: Image.network(images[index].url)
+Widget buildImageStyle(ImageModel image) {
+  return Container(
+    decoration: BoxDecoration(
+      border: Border.all(color: Colors.pink[300]!),
+    ),
+    padding: EdgeInsets.all(20.0),
+    margin: EdgeInsets.all(20.0),
+    // to show something inside a conatainer, asign it to the containers child property, but the container expects to only recieve one widgert, so to show two things they must be wrapped in a
+    child: Column(
+      // the children list will only contain <Widget> type elements
+      children: <Widget>[
+        Padding(
+          child: Image.network(image.url),
+          padding: EdgeInsets.only(bottom: (10.0)),
+        ),
+        Text(image.title),
+      ],
+    ),
+  );
+}
