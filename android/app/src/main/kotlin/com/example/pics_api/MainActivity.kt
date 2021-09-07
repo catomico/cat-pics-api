178425623d546c2f215478e03b89b67abc2066bb
user86 <user86marc@gmail.com>
2021-09-07 07:27:34 +0200

    initialise

--- android/app/src/main/kotlin/com/example/pics_api/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.pics_api
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++
@@ -1,6 +0,0 @@
-// import a helper lib from flutter to get content on the screen
-import 'package:flutter/material.dart';
-import 'src/app.dart';
-
-// define a 'main' function, and run it
-void main() => runApp(AppWidget());
